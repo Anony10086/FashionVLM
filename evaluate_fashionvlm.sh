c673db0f2ac7a2e1d85093ce16eb5de534fb1967
anoyn <anoyn@email.com>
2025-10-10 17:24:00 +0800

    UPDATE:  evaluate scaript. move Evaluation process to the end

--- evaluate_fashionvlm.sh
+++ evaluate_fashionvlm.sh
@@ -21,12 +21,21 @@ for TASK_NAME in "${TASKS[@]}"; do
     # 2. Generate Image
     echo "${i}.2. Generating $METHOD_NAME $TASK_NAME Image"
     python3 evaluate/generate_image_fashionvlm.py --method "$METHOD_NAME" --task "$TASK_NAME"
-    
-    # 3. Evaluate
-    echo "${i}.3. Evaluating $METHOD_NAME on $TASK_NAME"
-    python3 evaluate/evaluate_multiple_tasks.py --method "$METHOD_NAME" --task "$TASK_NAME"
 
     echo "--- Task $TASK_NAME Complete ---"
+    echo "--------------------------------------------------"
+    i=$((i + 1))
+done
+
+i=1
+for TASK_NAME in "${TASKS[@]}"; do
+    echo "--- INFERENCE COMPLETE, START EVALUATION ---"
+    
+    # 3. Evaluate
+    echo "${i}. Evaluating $METHOD_NAME on $TASK_NAME"
+    python3 evaluate/evaluate_multiple_tasks.py --method "$METHOD_NAME" --task "$TASK_NAME"
+
+    echo "--- Task $TASK_NAME Evaluation Complete ---"
     echo "--------------------------------------------------"
     i=$((i + 1))
 done
